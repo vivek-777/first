ec280bd3b9fc568386cf5a73dbe574ba0ab03a60
vivek-777 <40752803+vivek-777@users.noreply.github.com>
2018-07-05 01:28:31 +0530

    Update views.py

--- first_app/views.py
+++ first_app/views.py
@@ -17,7 +17,7 @@ def predict_marks(request):
 #implement machine learning
 #stage1-read data into data frame
     import pandas as pd
-    df=pd.read_csv("first/first_app/Grade_Set.csv")
+    df=pd.read_csv("C:\Users\HP\Desktop\first\first_app\Grade_Set.csv")
 #stage 2-->>>selection
     import numpy as np
     x=df.Hours_Studied[:,np.newaxis]#independent
